6f9c1f095215b08cdf1c26e0469000b2fef4d5a1
foressta <tsuffer529@gmail.com>
2025-09-21 15:21:56 +0900

    Refactored: Replaced media queries with clamp() and corrected imporoper h2 tag to div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,22 @@ import Image from "next/image";
 const Home = () => {
   return ( 
     <div className="flex justify-center items-center min-h-screen">
-      <main className="p-300 rounded-[20px] space-y-300 max-w-[327px] sm:max-w-[384px] border border-gray-950 shadow-[8px_8px_0_rgba(0,0,0,1)] bg-white">
+      <main className="p-300 rounded-[20px] space-y-300 max-w-[clamp(20.438rem,19.183rem+5.35vw,24rem)] border border-gray-950 shadow-[8px_8px_0_rgba(0,0,0,1)] bg-white">
         <Image
           src={"/images/illustration-article.svg"}
           alt="illustration-article"
           width={327}
           height={200}
-          quality={100}
           className="rounded-[10px]"
         />
         <section className="space-y-150">
-          <h2 className="typo-4 px-150 py-50 rounded inline-block bg-yellow text-gray-950">
+          <div className="typo-4 px-150 py-50 rounded inline-block bg-yellow text-gray-950">
             Learning
-          </h2>
+          </div>
           <p className="typo-3 text-gray-950">
             Published <time dateTime="2023-12-21">21 Dec 2023</time>
           </p>
-          <h1 className="typo-1 text-gray-950 hover:text-yellow">
+          <h1 className="typo-1 text-gray-950 hover:text-yellow hover:cursor-pointer">
             HTML & CSS foundations
           </h1>
           <p className="typo-2 text-gray-500">
@@ -33,7 +32,7 @@ const Home = () => {
             alt="avatar"
             width={32}
             height={32}
-            quality={100}
+
           />
           <span className="typo-4">Greg Hooper</span>
         </div>
